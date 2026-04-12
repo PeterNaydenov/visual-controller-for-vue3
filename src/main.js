303837697edac5243eaf0a2eae02375223d89517
Peter Naydenov <peter.naydenov@gmail.com>
2026-04-12 21:17:58 +0300

    Refactoring
    - Convert from 'class' to 'functional' approach;
    - Adding 'jsdoc' description to the library;
    -  Updated Vue config to use compilerOptions.isCustomElement (non-deprecated API in Vue 3.3+)

--- src/main.js
+++ src/main.js
@@ -7,41 +7,47 @@
 import askForPromise from 'ask-for-promise'
 import { createApp, createSSRApp } from 'vue'
 
-class VisualController {
 
-    constructor ( dependencies = {} ) {
-              const cache = {}  // collect vue-component update interfaces
-              this.dependencies = { ...dependencies, createApp, createSSRApp }
-              return {
-                          publish : this.publish ( this.dependencies, cache )
-                        , destroy : this.destroy ( cache )
-                        , getApp  : this.getApp  ( cache )
-                        , has     : id => cache[id] ? true : false
-                    }
-        }
+
+function VisualController ( dependencies = {} ) {
+    /** @type {Object.<string, any>} */
+    const cache = {}  // collect vue-component update interfaces
+    
+    
+    dependencies = { ...dependencies, createApp, createSSRApp }
 
 
 
-    publish ( dependencies, cache ) {
-        return function (component, data, id) {
+    /**
+     * Publish vue app in container.
+     * @param {Object} component Vue component
+     * @param {Object} data Data object
+     * @param {boolean} [data.isCustomElement] Whether it is a custom element
+     * @param {string} id Id of the container where vue-app will live.
+     * @returns {Promise<Object>} Promise with an object containing update interface methods
+     */
+    function publish ( component, data={}, id ) {
+                    
                     const 
-                          hasKey = this.has ( id )
-                        , { createApp, createSSRApp } = dependencies
+                          hasKey = typeof id === 'string' 
                         , { isCustomElement } = data
                         , endTask = askForPromise ()
                         ;
+                    
                     if ( !component ) {
                             console.error ( `Error: Component is undefined` )
                             endTask.done (false)
                             return endTask.promise
                     }
-                    if ( hasKey )   this.destroy ( id )
+                    if ( hasKey )   destroy ( id )
 
                     let 
                           node = document.getElementById ( id )
                         , app 
-                        , setupUpdates = lib => cache[id] = lib
                         ;
+                    
+                    /** @type {function(any):void} */
+                    const setupUpdates = lib => { cache[id] = lib }
                     if ( !node ) {  
                                 console.error ( `Can't find node with id: "${id}"`)
                                 endTask.done ( false )
@@ -58,40 +64,71 @@ class VisualController {
                     cache[id] = {}
                     app.provide ( 'dependencies', {...dependencies, setupUpdates})
 
-                    if ( isCustomElement )   app.config.isCustomElement = isCustomElement   // Autonomous Custom Elements. Docs: https://v3.vuejs.org/guide/migration/custom-elements-interop.html#autonomous-custom-elements
+                    if ( isCustomElement ) {
+                            app.config.compilerOptions = app.config.compilerOptions || {}
+                            app.config.compilerOptions.isCustomElement = tag => isCustomElement
+                        }
                     app.mount (`#${id}`)
                     endTask.done ( cache[id] )
                     return endTask.promise
-            }} // publish func.
+        } // publish func.
 
 
 
-    destroy ( cache ) {
-        return function (id) {
+    /**
+     * Destroy app by using container name
+     * @param {string} id Id of the container where vue-app lives.
+     * @returns {boolean} True if app was destroyed, false otherwise.
+     */
+    function destroy  ( id ) {
                     if ( cache[id] ) {                    
-                            let 
-                                  node = document.getElementById ( id )
-                                , unmount = node.__vue_app__.unmount
-                                ;
+                            /** @type {any} */
+                            const node = document.getElementById ( id )
+                            if ( !node )    return false
+                            const unmount = node.__vue_app__.unmount
                             unmount ()
                             node.removeAttribute ( 'data-v-app' )
                             delete cache[id]
                             return true
                         }
                     else    return false
-            }} // destroy func.
+            } // destroy func.
 
 
             
-    getApp ( cache ) {
-        return function (id) {
+    /**
+     * Returns app instance by container name
+     * @param {string} id Id of the container where vue-app lives.
+     * @returns {object|false} App instance if found, false otherwise.
+     */
+    function getApp (id) {
                 const item = cache[id];
                 if ( !item ) {  
                         console.error ( `App with id: "${id}" was not found.`)
                         return false
                     }
                 return item
-        }} // getApp func.
+        } // getApp func.
+
+
+
+    /**
+     * Check if app with specific "id" was published
+     * @param {string} id Id of the container where vue-app lives.
+     * @returns {boolean} True if app with "id" was published, false otherwise.
+     */
+    function has ( id ) {
+            return cache[id] ? true : false
+        } // has func.
+ 
+
+    
+    return {
+              publish
+            , destroy 
+            , getApp  
+            , has
+        }
 } // visualController
 
 
