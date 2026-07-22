7f2a30eee83523a9d7237bec39f1f1265b5ee0f1
Peter Naydenov <peter.naydenov@gmail.com>
2026-07-22 06:50:10 +0300

    Type updates

--- dist/main.d.ts
+++ dist/main.d.ts
@@ -1,7 +1,7 @@
-export default VisualController;
 /**
  *  @function VisualController - Visual Controller for Vue 3
  *  @param {Object} [dependencies={}] - Optional external dependencies to inject into Vue apps
  *  @returns {Object} Controller instance with publish, destroy, getApp, and has methods
  */
 declare function VisualController(dependencies?: any): any;
+export default VisualController;
